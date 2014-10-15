0fea40e8197960486cadf2b2fc52dc75c3174d15
zacatac <field.zackery@gmail.com>
2014-10-14 22:51:43 -0700

    updated urls.py in emailonly backend to import the views defined in emailonly.views

--- registration/backends/emailonly/urls.py
+++ registration/backends/emailonly/urls.py
@@ -22,8 +22,8 @@ from django.conf.urls import include
 from django.conf.urls import url
 from django.views.generic.base import TemplateView
 
-from registration.backends.default.views import ActivationView
-from registration.backends.default.views import RegistrationView
+from registration.backends.emailonly.views import ActivationView
+from registration.backends.emailonly.views import RegistrationView
 
 
 urlpatterns = patterns('',
